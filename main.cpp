5a8b5ac858e3da8d756c49475bfa225b6b804be7
Dawyla Maria <dawylamaria@gmail.com>
2023-08-15 12:50:46 -0300

    questao3

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/* Questao3*/ 
+
+using namespace std;
+int main(int argc, char** argv) {
+	int num1;
+	int num2;
+	
+	cout <<"Digite o primeiro numero:" << endl;
+	
+	cin >> num1;
+	
+	cout <<"Digite o segundo numero:"<< endl;
+	
+	cin >> num2;
+	
+	cout <<"Soma:" << num1 + num2 << endl;
+	system("pause");
+	return 0;
+}
